75c2f00951e27b4900a61baaf6e8aa62ded5a2e4
Aryan Kumar Shrivastav <main.aryanshrivastav2003@gmail.com>
2026-02-06 23:21:09 +0530

    Setting up project, installing dependencies and starting server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+require('dotenv').config()
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server Started at ", process.env.PORT)
+})
